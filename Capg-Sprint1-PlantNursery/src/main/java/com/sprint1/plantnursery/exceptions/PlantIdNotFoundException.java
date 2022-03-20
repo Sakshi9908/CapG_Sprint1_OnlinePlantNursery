08e8bfdba384e16756481faec0477ef2b41de13a
Sakshi9908 <sakshishah773@gmail.com>
2022-03-20 21:32:51 +0530

    Sprint 1 CaseStudy Project Added

---
+++ Capg-Sprint1-PlantNursery/src/main/java/com/sprint1/plantnursery/exceptions/PlantIdNotFoundException.java
@@ -0,0 +1,33 @@
+package com.sprint1.plantnursery.exceptions;
+
+/*Controller Class for Customer Controller
+Author : Disha Kale
+*/
+
+public class PlantIdNotFoundException extends RuntimeException {
+	public PlantIdNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PlantIdNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PlantIdNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PlantIdNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PlantIdNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
